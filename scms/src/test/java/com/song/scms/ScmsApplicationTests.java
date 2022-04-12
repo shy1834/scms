8c7ed4b67a66132b71aeecb720bb26aa9ea79083
ssong <shy1834@gmail.com>
2022-04-12 21:07:05 +0900

    처음 test

---
+++ scms/src/test/java/com/song/scms/ScmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.scms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
